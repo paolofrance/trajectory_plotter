97f0180382cd3a4bc8dd5ad501e0d596d63c6255
Paolo Franceshi <paolo.franceshi@stiima.cnr.it>
2022-06-27 12:25:55 +0200

    paolo

--- script/ramp_setpoint.py
+++ script/ramp_setpoint.py
@@ -4,68 +4,83 @@ import copy
 # import moveit_commander
 import numpy as np
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, WrenchStamped
 from geometry_msgs.msg import Pose
+import random
 
 
 class current_pos_monitor:
 
     def __init__(self):
         self.current_pose = Pose()
+        self.wrench = WrenchStamped()
 
     def current_callback(self, data):
         self.current_pose = data.pose
+
+    def wrench_callback(self, data):
+        self.wrench = data
 
 
 def traj_publisher():
     rospy.init_node('traj_publisher')
 
     nominal_traj_topic = rospy.get_param('nominal_traj_topic')
-    human_traj_topic   = rospy.get_param('human_traj_topic'  )
     current_traj_topic = rospy.get_param('current_traj_topic')
+    human_traj_topic = rospy.get_param('human_traj_topic')
     rospy.sleep(.1)
 
     cpm = current_pos_monitor()
     current_sub = rospy.Subscriber(current_traj_topic, PoseStamped, cpm.current_callback)
+    wrench_sub = rospy.Subscriber("filtered_wrench_base", WrenchStamped, cpm.wrench_callback)
 
     nom_pub = rospy.Publisher(nominal_traj_topic, PoseStamped, queue_size=10)
     hum_pub = rospy.Publisher(human_traj_topic, PoseStamped, queue_size=10)
     rate = 125.0
     ros_rate = rospy.Rate(rate)
-    set_point = 0.3
-    human_set_point = 0.6
-    rospy.sleep(1)
+    set_point = 0.6
+    # set_point = random.uniform(.4, .9)
+    # duration = set_point/2
+    duration = 0.3
+    rospy.sleep(.5)
+
 
     reference_pose = copy.deepcopy(cpm.current_pose)
-    human_reference_pose = copy.deepcopy(cpm.current_pose)
+    human_pose = copy.deepcopy(cpm.current_pose)
     initial_pose = copy.deepcopy(cpm.current_pose)
-    reference_pose.position.x = initial_pose.position.x + set_point
+    # reference_pose.position.x = initial_pose.position.x + set_point
     # reference_pose.position.y = initial_pose.position.y + set_point
     # reference_pose.position.z = initial_pose.position.z + set_point
-    human_reference_pose.position.x = initial_pose.position.x + human_set_point
+
+    goal_pose = copy.deepcopy(cpm.current_pose)
+    goal_pose.position.z =  initial_pose.position.z + set_point
 
     reference_pose_msg = PoseStamped()
     human_pose_msg = PoseStamped()
 
+    count =0
+    start_setpoint=False
+
     while not rospy.is_shutdown():
 
-        # if abs(cpm.current_pose.position.x-reference_pose.position.x) < 0.005:
-        #     set_point = -set_point
-        #     reference_pose.position.x = initial_pose.position.x + set_point
-        #     print("change")
+        if abs(cpm.wrench.wrench.force.z) > 0.5:
+            start_setpoint = True
 
-        # if abs(cpm.current_pose.position.z - reference_pose.position.z) < 0.001:
-        #     set_point = -set_point
-        #     reference_pose.position.z = initial_pose.position.z + set_point
-        #     print("change")
-
-        stamp = rospy.Time.now()
+        if start_setpoint:
+            count = count+1
+            if abs(reference_pose.position.z - goal_pose.position.z) > 0.05:
+                reference_pose.position.z = initial_pose.position.z + set_point/duration * count/rate
+            else:
+                reference_pose.position.z = initial_pose.position.z + set_point
+        else:
+            reference_pose.position.z = initial_pose.position.z
 
         reference_pose_msg.pose = reference_pose
+        stamp = rospy.Time.now()
         reference_pose_msg.header.stamp = stamp
         nom_pub.publish(reference_pose_msg)
 
-        human_pose_msg.pose = human_reference_pose
+        human_pose_msg.pose = goal_pose
         human_pose_msg.header.stamp = stamp
         hum_pub.publish(human_pose_msg)
 
--- script/trajectory_plotter.py
+++ script/trajectory_plotter.py
@@ -25,30 +25,38 @@ class Plotter:
 
         self.x_nom = np.array([])
         self.y_nom = np.array([])
+        self.z_nom = np.array([])
 
         self.x_init = 0.0
         self.y_init = 0.0
+        self.z_init = 0.0
         self.init_pose = True
 
         self.x_cur = np.array([])
         self.y_cur = np.array([])
+        self.z_cur = np.array([])
 
         self.x_hum = np.array([])
         self.y_hum = np.array([])
+        self.z_hum = np.array([])
 
         self.x_0 = 0
         self.y_0 = 0
+        self.z_0 = 0
         self.pose_zeroing = True
 
         self.circle1 = plt.Circle((0.16, -0.16), 0.03, color='r')
 
     def nominal_callback(self, data):
         if self.init_pose:
+            rospy.sleep(0.1)
             self.x_init = data.pose.position.x
             self.y_init = data.pose.position.y
+            self.z_init = data.pose.position.z
             self.init_pose = False
         self.x_nom = data.pose.position.x
         self.y_nom = data.pose.position.y
+        self.z_nom = data.pose.position.z
         self.show_nom = True
 
     def current_callback(self, data):
@@ -56,16 +64,19 @@ class Plotter:
         if self.pose_zeroing:
             self.x_0 = data.pose.position.x
             self.y_0 = data.pose.position.y
+            self.z_0 = data.pose.position.z
             self.pose_zeroing = False
 
         self.x_cur = data.pose.position.x
         self.y_cur = data.pose.position.y
+        self.z_cur = data.pose.position.z
 
         self.show_cur = True
 
     def human_callback(self, data):
         self.x_hum = data.pose.position.x
         self.y_hum = data.pose.position.y
+        self.z_hum = data.pose.position.z
         self.show_hum = True
 
     def add_circle(self, x, y):
@@ -77,8 +88,10 @@ if __name__ == '__main__':
     current_traj_topic = rospy.get_param('current_traj_topic')
     human_traj_topic   = rospy.get_param('human_traj_topic'  )
 
-    delta_axis = [-0.3, 0.3, -0.3, 0.3] # centered
     delta_axis = [0.1, 0.6, 0.3, 0.3] # left
+    delta_axis = [0.1, 0.1, 0.1, 0.9] # load - Z
+    delta_axis = [0.1, 0.7, 0.6, 0.6] # all view
+    # delta_axis = [0.4, 0.4, 0.1, 0.1] # all view
 
     fig, Ax = plt.subplots(1)
 
@@ -98,26 +111,30 @@ if __name__ == '__main__':
             #     circ = plt.Circle((my_plotter.x_nom, my_plotter.y_nom), 0.03, color='r')
             #     my_plotter.ax.add_patch(circ)
             my_plotter.show_nom = False
-            my_plotter.ax.plot(my_plotter.x_nom, my_plotter.y_nom, 'om')
+            # my_plotter.ax.plot(my_plotter.x_nom, my_plotter.y_nom, 'om')
+            my_plotter.ax.plot(my_plotter.x_nom, my_plotter.z_nom, 'om')
             plt.draw()
             plt.pause(0.001)
 
         if my_plotter.show_cur:
             my_plotter.show_cur = False
-            my_plotter.ax.plot(my_plotter.x_cur, my_plotter.y_cur, 'og')
+            # my_plotter.ax.plot(my_plotter.x_cur, my_plotter.y_cur, 'og')
+            my_plotter.ax.plot(my_plotter.x_cur, my_plotter.z_cur, 'og')
             plt.draw()
             plt.pause(0.001)
 
         if my_plotter.show_hum:
             my_plotter.show_hum = False
-            my_plotter.ax.plot(my_plotter.x_hum, my_plotter.y_hum, 'ob')
+            # my_plotter.ax.plot(my_plotter.x_hum, my_plotter.y_hum, 'ob')
+            my_plotter.ax.plot(my_plotter.x_hum, my_plotter.z_hum, 'ob')
             plt.draw()
             plt.pause(0.001)
 
-        if my_plotter.x_init-0.02 < my_plotter.x_nom < my_plotter.x_init+0.02 and my_plotter.y_init-0.02 < my_plotter.y_nom < my_plotter.y_init+0.02 :
-            my_plotter.ax.clear()
+        # if my_plotter.x_init-0.02 < my_plotter.x_nom < my_plotter.x_init+0.02 and my_plotter.y_init-0.02 < my_plotter.y_nom < my_plotter.y_init+0.02 :
+        #     my_plotter.ax.clear()
         my_plotter.ax.clear()
-        axis = [my_plotter.x_0 - delta_axis[0], my_plotter.x_0 + delta_axis[1], my_plotter.y_0 - delta_axis[2], my_plotter.y_0 + delta_axis[3]]
+        # axis = [my_plotter.x_0 - delta_axis[0], my_plotter.x_0 + delta_axis[1], my_plotter.y_0 - delta_axis[2], my_plotter.y_0 + delta_axis[3]]
+        axis = [my_plotter.x_0 - delta_axis[0], my_plotter.x_0 + delta_axis[1], my_plotter.z_0 - delta_axis[2], my_plotter.z_0 + delta_axis[3]]
         plt.axis(axis)
         plt.grid()
 
