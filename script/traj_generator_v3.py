6562815c5fd7d36a456fab3b44ea5596192431a1
fabio-bertini <fabio.bertini97@gmail.com>
2022-08-01 13:44:22 +0200

    -

--- script/traj_generator_v3.py
+++ script/traj_generator_v3.py
@@ -27,18 +27,18 @@ def traj_publisher():
     rospy.sleep(.1)
 
     nominal_traj_topic = rospy.get_param('nominal_traj_topic')
-    human_traj_topic   = rospy.get_param('human_traj_topic')
+    #human_traj_topic   = rospy.get_param('human_traj_topic')
     current_traj_topic = rospy.get_param('current_traj_topic')
     obst_x_coord_topic = rospy.get_param('obst_x_coord_topic')
     current_sub        = rospy.Subscriber(current_traj_topic  , PoseStamped, cpm.current_callback)
 
     print('\n PUBLISHED TOPICS:')
     print(nominal_traj_topic)
-    print(human_traj_topic)
+    #print(human_traj_topic)
     print(obst_x_coord_topic)
 
     nom_pub  = rospy.Publisher(nominal_traj_topic, PoseStamped, queue_size=10)
-    hum_pub  = rospy.Publisher(human_traj_topic  , PoseStamped, queue_size=10)
+    #hum_pub  = rospy.Publisher(human_traj_topic  , PoseStamped, queue_size=10)
     obst_pub = rospy.Publisher(obst_x_coord_topic, PoseStamped, queue_size=10)
     rate     = 125.0
     ros_rate = rospy.Rate(rate)
@@ -109,23 +109,22 @@ def traj_publisher():
         t += 1.0/rate
 
         # HUMAN POSE
-        hum_pose_msg = PoseStamped()
+        #hum_pose_msg = PoseStamped()
         # X (both human and nom):
         if case_x == 1:             # X as cosine
             if t < wait:   # wait 3 seconds
-                hum_pose_msg.pose.position.x = initial_pose.position.x
+                #hum_pose_msg.pose.position.x = initial_pose.position.x
                 reference_pose.position.x    = initial_pose.position.x
             elif (t-wait) <= (0.5/cos_freq):
-                hum_pose_msg.pose.position.x = initial_pose.position.x + 0.5 * x5 * (1 - np.cos(omega * (t - wait)))
+                #hum_pose_msg.pose.position.x = initial_pose.position.x + 0.5 * x5 * (1 - np.cos(omega * (t - wait)))
                 reference_pose.position.x    = initial_pose.position.x + 0.5 * x5 * (1 - np.cos(omega * (t - wait)))
             elif (t - wait) <= (0.5 / cos_freq) + end_wait:
-                hum_pose_msg.pose.position.x = initial_pose.position.x + x5
+                #hum_pose_msg.pose.position.x = initial_pose.position.x + x5
                 reference_pose.position.x = initial_pose.position.x + x5
             elif (t-wait) <= (0.5 / cos_freq) + (x5 / vel_return) + end_wait:
                 if rec:
                     os.killpg(os.getpgid(record_process.pid), signal.SIGTERM)
-                hum_pose_msg.pose.position.x = initial_pose.position.x + x5 - \
-                                               (vel_return * (t - wait - (x5 / vel_case_x_2) - end_wait))
+                #hum_pose_msg.pose.position.x = initial_pose.position.x + x5 - (vel_return * (t - wait - (x5 / vel_case_x_2) - end_wait))
                 reference_pose.position.x = initial_pose.position.x + x5 - \
                                             (vel_return * (t - wait - (x5 / vel_case_x_2) - end_wait))
             else:
@@ -142,7 +141,7 @@ def traj_publisher():
 
         elif case_x == 2:           # X as constant velocity
             if t < wait:
-                hum_pose_msg.pose.position.x = initial_pose.position.x
+                #hum_pose_msg.pose.position.x = initial_pose.position.x
                 reference_pose.position.x    = initial_pose.position.x
                 # if ((vel_case_x_2 * t) // x5) % 2 == 0:  # even after floor
                 #     hum_pose_msg.pose.position.x = initial_pose.position.x
@@ -151,10 +150,10 @@ def traj_publisher():
                 #     hum_pose_msg.pose.position.x = initial_pose.position.x
                 #     reference_pose.position.x    = initial_pose.position.x
             elif (t-wait) <= (x5 / vel_case_x_2):
-                hum_pose_msg.pose.position.x = initial_pose.position.x + (vel_case_x_2 * (t - wait))
+                #hum_pose_msg.pose.position.x = initial_pose.position.x + (vel_case_x_2 * (t - wait))
                 reference_pose.position.x    = initial_pose.position.x + (vel_case_x_2 * (t - wait))
             elif (t-wait) <= (x5 / vel_case_x_2) + end_wait:
-                hum_pose_msg.pose.position.x = initial_pose.position.x + x5
+                #hum_pose_msg.pose.position.x = initial_pose.position.x + x5
                 reference_pose.position.x    = initial_pose.position.x + x5
             #     if ((vel_case_x_2 * t) // x5) % 2 == 0:    # even after floor
             #         hum_pose_msg.pose.position.x = initial_pose.position.x + (vel_case_x_2 * (t - wait)) % x5
@@ -165,7 +164,7 @@ def traj_publisher():
             elif (t-wait) <= (x5 / vel_case_x_2) + (x5 / vel_return) + end_wait:
                 if rec:
                     os.killpg(os.getpgid(record_process.pid), signal.SIGTERM)
-                hum_pose_msg.pose.position.x = initial_pose.position.x + x5 - (vel_return * (t - wait - (x5 / vel_case_x_2) - end_wait))
+                #hum_pose_msg.pose.position.x = initial_pose.position.x + x5 - (vel_return * (t - wait - (x5 / vel_case_x_2) - end_wait))
                 reference_pose.position.x = initial_pose.position.x + x5 - (vel_return * (t - wait - (x5 / vel_case_x_2) - end_wait))
             else:
                 run = input('Press enter to continue, else digit "stop" \n')
@@ -180,24 +179,24 @@ def traj_publisher():
                         print("Script is running... \n")
 
         # human target Y:
-        hum_pose_msg.pose.position.y = initial_pose.position.y
+        #hum_pose_msg.pose.position.y = initial_pose.position.y
 
         # REFERENCE POSITION Y
         reference_pose.position.y    = initial_pose.position.y
         reference_pose.position.z    = initial_pose.position.z
-        hum_pose_msg.pose.position.z = initial_pose.position.z
+        #hum_pose_msg.pose.position.z = initial_pose.position.z
 
         # Obstacle y coordinate
         obst_pose_msg.pose.position.y = 0
 
         stamp = rospy.Time.now()
-        hum_pose_msg.header.stamp = stamp
+        #hum_pose_msg.header.stamp = stamp
         obst_pose_msg.header.stamp = stamp
         nom_pose_msg              = PoseStamped()
         nom_pose_msg.pose         = reference_pose
         nom_pose_msg.header.stamp = stamp
 
-        hum_pub.publish(hum_pose_msg)
+        #hum_pub.publish(hum_pose_msg)
         nom_pub.publish(nom_pose_msg)
         obst_pub.publish(obst_pose_msg)
         ros_rate.sleep()
